075926647791b5ecc5355bc4326bbcf8ad779c3b
Felipe <sbuttonifelipe@gmail.com>
2026-04-11 19:03:48 -0300

    agrego constructor a la entidad 'Product.cs'

--- fsbuttoni/libreria/src/Domain/Entities/Product.cs
+++ fsbuttoni/libreria/src/Domain/Entities/Product.cs
@@ -5,4 +5,11 @@ public class Product
     public int Id { get; set; }
     public string Name { get; set; }
     public decimal Price { get; set; }
+
+    public Product(int id, string name, decimal price)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+    }
 }
